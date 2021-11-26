6b64872b6e944d8fb70a399deafe84ce71db4b21
geraldelorm <geraldelorm14@gmail.com>
2021-11-26 16:21:53 +0000

    setpu

---
+++ src/main/java/io/turntabl/tsrs/TsRsApplication.java
@@ -0,0 +1,13 @@
+package io.turntabl.tsrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsRsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsRsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/turntabl/tsrs/TsRsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.turntabl.tsrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsRsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
