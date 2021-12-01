2f322c8e601ce9dd854acde897499766124bab7e
eddykofi1500 <eddykofi1500@gmail.com>
2021-12-01 16:54:07 +0000

    endpoint for admin accessing transactions

---
+++ src/main/java/io/turntabl/tsrs/controller/Reportcontroller.java
@@ -0,0 +1,26 @@
+package io.turntabl.tsrs.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RequestMapping("/orderApi")
+@RestController
+public class Reportcontroller {
+    @Autowired
+    private RestTemplate restTemplate;
+    private static String url = "https://exchange.matraining.com/orderbook/";
+
+    @GetMapping("/{product}/{order_type}")
+    public List<List> BuyOrderProduct(@PathVariable String product,@PathVariable String order_type){
+        List forObject = restTemplate.getForObject(url+"/"+product+"/"+order_type, List.class);
+        return Arrays.asList(forObject);
+    }
+
+}
