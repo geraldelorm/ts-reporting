dd00825196559a0fa3d6cf2bcec0f1a75da7af32
geraldelorm <geraldelorm14@gmail.com>
2021-12-02 18:42:27 +0000

    res

--- src/main/java/io/turntabl/tsrs/controller/HomeController.java
+++ src/main/java/io/turntabl/tsrs/controller/HomeController.java
@@ -1,10 +1,10 @@
-package io.turntabl.tsrs;
+package io.turntabl.tsrs.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Controller {
+public class HomeController {
 
     @GetMapping
     public String home(){
